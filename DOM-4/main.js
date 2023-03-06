80835a838db417b6778f043f9db122e565ae05c2
Ariful Asfake Sunny <92142951+AsfaSunny@users.noreply.github.com>
2023-03-06 22:37:58 +0600

    Add files via upload

---
+++ DOM-4/main.js
@@ -0,0 +1,74 @@
+// var a = document.getElementById('inner-div').parentElement;
+// var a = document.getElementById('main').parentElement;
+// var a = document.getElementById('child-C').parentElement;
+// console.log(a);
+
+
+// document.getElementById('outer-div').parentNode.style.background = "indigo";
+// document.getElementById('outer-div').parentElement.style.background = "crimson";
+
+
+// var b = document.getElementById('child-C').parentNode;
+// var b = document.getElementById('main').parentNode;
+// var b = document.getElementById('outer-div').parentNode;
+// console.log(b);
+
+
+
+
+
+
+
+// document.getElementById('inner-div').children[1].style.background = 'orange';
+// document.getElementById('inner-div').childNodes[5].style.background = 'purple';
+// // var c = document.getElementById('inner-div').children;
+// // var c = document.getElementById('inner-div').children[0].innerHTML;
+// var c = document.getElementById('inner-div').childNodes;
+// console.log(c);
+
+
+
+
+
+
+
+
+// document.getElementById('inner-div').firstElementChild.style.background = 'crimson';
+// document.getElementById('inner-div').firstChild.style.background = 'crimson';
+// document.getElementById('inner-div').lastElementChild.style.background = 'crimson';
+// document.getElementById('inner-div').lastChild.style.background = 'crimson';
+
+
+// var d = document.getElementById('inner-div').lastElementChild;
+// var d = document.getElementById('inner-div').lastChild;
+// var d = document.getElementById('inner_header').lastChild;
+// var d = document.getElementById('inner_header').lastElementChild;
+// console.log(d);
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('child-D').nextSibling.style.background = 'crimson';
+document.getElementById('child-D').previousElementSibling.style.background = 'crimson';
+document.getElementById('child-D').nextElementSibling.style.background = 'crimson';
+// document.getElementById('child-D').previousSibling.style.background = 'crimson';
+
+
+// var e = document.getElementById('child-E').previousElementSibling;
+// var e = document.getElementById('child-E').nextElementSibling;
+// var e = document.getElementById('child-D').previousSibling;
+var e = document.getElementById('child-E').nextSibling;
+console.log(e);
+
+
+
+
+
+
